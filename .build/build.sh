30e8a47f8552f04303ff8c079ff11a1724b0e053
Dayle Rees <thepunkfan@gmail.com>
2017-12-15 23:44:31 +0000

    Better theme names.

--- .build/build.sh
+++ .build/build.sh
@@ -4,10 +4,10 @@
 docker run -e PATTERN=sublime -v $PWD/output/:/src/output daylerees/rainglow:latest
 
 # Kill old themes.
-rm -rf ../themes/*.tmTheme
+rm -rf ../rainglow/*.tmTheme
 
 # Move themes to the root.
-cp -R output/sublime/* ../themes/
+cp -R output/sublime/* ../rainglow/
 
 # Delete temp themes.
 rm -rf output
